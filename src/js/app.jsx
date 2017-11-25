e8d3373a5506da2e033bc8dee87c51627ca90ab9
nicolasvasquez <transeunte.123@gmail.com>
2017-11-24 23:30:12 -0300

    Commit inicial

---
+++ src/js/app.jsx
@@ -0,0 +1,17 @@
+require('./bootstrap');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router} from 'react-router-dom';
+import {store} from './store/configure';
+import Home from './components/Home/Home'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <Home />
+        </Router>
+    </Provider>, 
+    document.getElementById('app')
+);
---
+++ src/js/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div align="center">
+                <h1>HOLA</h1>
+                <br/>
+            </div>
+        );
+    }
+}
